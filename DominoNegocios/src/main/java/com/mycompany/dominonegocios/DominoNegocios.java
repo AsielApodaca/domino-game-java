b602d0b677bf75195b6e0aa97e0903c9eb7befef
Oliver-IV <145882378+Oliver-IV@users.noreply.github.com>
2024-10-01 21:49:39 -0700

    Initial Commit

---
+++ DominoNegocios/src/main/java/com/mycompany/dominonegocios/DominoNegocios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dominonegocios;
+
+/**
+ *
+ * @author olive
+ */
+public class DominoNegocios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ DominoPresentacion/src/main/java/dev/itson/dominopresentacion/tablerodomino/TableroDominoModel.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.itson.dominopresentacion.tablerodomino;
+
+/**
+ *
+ * @author olive
+ */
+public class TableroDominoModel {
+    
+}
