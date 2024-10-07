fb2136513fc838a17352b90e329d2700e391466c
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2024-10-07 00:02:55 -0700

    Renombre paquete de mapeadoDTO

---
+++ DominoDTO/src/main/java/mapeodto/MapeadorDTO.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mapeodto;
+
+import dominio.FichaDominoEntity;
+import dominio.PosicionEntity;
+import dominio.TableroDominoEntity;
+import dominodto.FichaDominoDTO;
+import dominodto.PosicionDTO;
+import dominodto.TableroDominoDTO;
+
+/**
+ *
+ * @author asielapodaca
+ */
+public class MapeadorDTO {
+    public FichaDominoDTO fichaEntityADTO(FichaDominoEntity ficha) {
+        int extremo1 = ficha.getExtremo1();
+        int extremo2 = ficha.getExtremo2();
+        
+        return new FichaDominoDTO(extremo1, extremo2);
+        
+    }
+    
+    public PosicionDTO posicionEntityADTO(PosicionEntity posicion) {
+        PosicionDTO posicionDTO = new PosicionDTO();
+        
+        posicionDTO.setPosicionX(posicion.getPosicionX());
+        posicionDTO.setPosicionY(posicion.getPosicionY());
+        posicionDTO.setRotacion(posicion.getRotacion());
+        posicionDTO.setAnchoPosicion(posicion.getAlturaPosicion());
+        posicionDTO.setAlturaPosicion(posicion.getAlturaPosicion());
+        FichaDominoDTO fichaDominoDTO = fichaEntityADTO(posicion.getFichaDomino());
+        if(fichaDominoDTO != null) {
+            posicionDTO.setFichaDominoDTO(fichaDominoDTO);
+        }
+        
+        return posicionDTO;
+    }
+    
+    public TableroDominoDTO tableroDominoEntityADTO(TableroDominoEntity tableroDomino) {
+        TableroDomino
+    }
+}
