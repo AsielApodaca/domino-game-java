63afafcadf7adb0db30c853c0a361e9da5e82c82
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2024-10-06 23:29:49 -0700

    Comienzo de modelo PartidaDomino

--- DominoPresentacion/src/main/java/partidadomino/PartidaDominoModel.java
+++ DominoPresentacion/src/main/java/partidadomino/PartidaDominoModel.java
@@ -4,6 +4,9 @@
  */
 package partidadomino;
 
+import dominodto.FichaDominoDTO;
+import java.util.List;
+
 /**
  *
  * @author asielapodaca
@@ -20,9 +23,17 @@ public class PartidaDominoModel {
     private int anchoFichaJugadorExterno; // ancho de las fichas de los jugadores externos.
     private int alturaFichaJugadorExterno; // altura de las fichas de los jugadores externos.
     
+    private int anchoTablero; // ancho del panel del tablero donde iran las fichas colocadas.
+    private int alturaTablero; // altura del panel del tablero donde iran las fichas colocadas.
+    private int tableroLocacionX; // locación del tablero en el eje de las X
+    private int tableroLocacionY; // locación del tablero en el eje de las Y
+    
     // Configuración de la partida
     private int numeroDeJugadores; // Número de jugadores dentro de la partida
     
     // Partida
+    private List<FichaDominoDTO> listaFichasJugadorLocal; // Lista de fichas del jugador del disposivo
+    //private List<FichaDominoDTO>[] listasFichasJugadoresExternos; // Listas de fichas de los jugadores externos (Temporal, posiblemente se cambie por lista jugadores externos)
+    
     
 }
