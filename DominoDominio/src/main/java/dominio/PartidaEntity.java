a65115c24ccc0116876b30e92eb9c468b528ecb3
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2024-10-11 14:10:37 -0700

    Create PartidaEntity.java

---
+++ DominoDominio/src/main/java/dominio/PartidaEntity.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio;
+
+/**
+ *
+ * @author asielapodaca
+ */
+public class PartidaEntity {
+    
+    private JugadorDominoEntity ganador; // Ganador de la partida
+    private JugadorDominoEntity turnoDeJugador; // Jugador con el turno de colocar ficha
+
+    public PartidaEntity(JugadorDominoEntity ganador, JugadorDominoEntity turnoDeJugador) {
+        this.ganador = ganador;
+        this.turnoDeJugador = turnoDeJugador;
+    }
+
+    public JugadorDominoEntity getGanador() {
+        return ganador;
+    }
+
+    public void setGanador(JugadorDominoEntity ganador) {
+        this.ganador = ganador;
+    }
+
+    public JugadorDominoEntity getTurnoDeJugador() {
+        return turnoDeJugador;
+    }
+
+    public void setTurnoDeJugador(JugadorDominoEntity turnoDeJugador) {
+        this.turnoDeJugador = turnoDeJugador;
+    }
+    
+}
