1b42b6889f2a4936e85c9c31790db15bd32565c2
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-11-19 22:18:54 -0700

    ServidotSolicitudFichaSeleccionada incompleto

--- PartidaServidor/src/main/java/controlSolicitudes/ServidorSolicitudFichaSeleccionada.java
+++ PartidaServidor/src/main/java/controlSolicitudes/ServidorSolicitudFichaSeleccionada.java
@@ -1,7 +1,13 @@
 package controlSolicitudes;
 
+import adapterEntidades.AdapterFichaDomino;
+import dominio.FichaDominoEntity;
+import dominio.SalaEntity;
+import dominio.TableroDominoEntity;
 import domino.solicitudes.EventoSolicitud;
 import domino.solicitudes.SolicitudFichaSeleccionada;
+import dominodto.FichaDominoDTO;
+import java.util.List;
 
 /**
  *
@@ -11,7 +17,19 @@ import domino.solicitudes.SolicitudFichaSeleccionada;
  * @author Asiel Apodaca Monge
  */
 public class ServidorSolicitudFichaSeleccionada extends SolicitudManejador {
+    private AdapterFichaDomino adapterFicha;
+    private TableroDominoEntity tableroDominoEntity;
+    private SalaEntity salaEntity;
+    
 
+
+    public ServidorSolicitudFichaSeleccionada() {
+        this.adapterFicha = new AdapterFichaDomino();
+        this.tableroDominoEntity = new TableroDominoEntity();
+        this.salaEntity = new SalaEntity();
+    }
+
+    
     /**
      * Procesa una solicitud de tipo SolicitudFichaSeleccionada o la
      * delega al siguiente manejador en la cadena de responsabilidad.
@@ -22,10 +40,30 @@ public class ServidorSolicitudFichaSeleccionada extends SolicitudManejador {
     @Override
     public void manejarSolicitud(EventoSolicitud solicitud) {
         if (solicitud instanceof SolicitudFichaSeleccionada solicitudFichaSeleccionada) {
-
+            onFichaSeleccionada(solicitudFichaSeleccionada);
         } else if (siguiente != null) {
             siguiente.manejarSolicitud(solicitud);
         }
     }
+    
+          
+    public void onFichaSeleccionada(SolicitudFichaSeleccionada eventoSolicitud) {
+        FichaDominoDTO fichaSeleccionadaDTO = eventoSolicitud.getFichaDominoDTO();
+        if (fichaSeleccionadaDTO == null) {
+            this.tableroDominoEntity.setFichaSeleccionada(null);
+            ocultarPosiblesMovimientos();
+        } else {
+            List<FichaDominoEntity> listaFichasJugador = salaEntity.getJugadorLocal().getListaFichasJugador();
+            FichaDominoEntity fichaSeleccionadaEntity
+                    = // compara la fichaDTO con las fichas del jugadorLocal
+                    estrategiaComparadorEntidades.comparar(
+                            fichaSeleccionadaDTO, listaFichasJugador
+                    );
+            this.tableroDominoEntity.setFichaSeleccionada(fichaSeleccionadaEntity);
+            System.out.println(tableroDominoEntity.getFichaSeleccionada() == null);
+            mostrarPosiblesMovimientos();
+        }
+    }
 
+    
 }
