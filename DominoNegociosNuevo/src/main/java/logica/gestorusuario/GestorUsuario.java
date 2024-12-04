7536d4d11524d175440fc5944582f0a783777a6f
Wogcas <gael.castro247887@potros.itson.edu.mx>
2024-12-03 20:35:25 -0700

    Gestor Usuario

---
+++ DominoNegociosNuevo/src/main/java/logica/gestorusuario/GestorUsuario.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica.gestorusuario;
+
+import dominio.UsuarioEntity;
+
+/**
+ *
+ * @author Hisamy Cinco Cota
+ * @author Gael Rafael Castro Molina
+ * @author Oliver Inzunza Valle
+ * @author Asiel Apodaca Monge
+ */
+public class GestorUsuario implements IGestorUsuario {
+
+    private UsuarioEntity usuarioActual;
+
+    @Override
+    public UsuarioEntity crearUsuario(String nombre, String rutaIcono) {
+        usuarioActual = new UsuarioEntity(nombre);
+        usuarioActual.setFuenteIcono(rutaIcono);
+        return usuarioActual;
+    }
+
+    @Override
+    public UsuarioEntity getUsuario() {
+        return usuarioActual;
+    }
+
+}
